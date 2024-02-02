c4d8f06df7361dbed6ce2521a64967ebfe6226eb
RealCommonCat <128367240+RealCommonCat@users.noreply.github.com>
2024-02-02 23:33:45 +0800

    更新了django测试代码

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-# myapp/models.py
+# myapp/tests.py
+from django.test import TestCase
+from django.urls import reverse
 from django.db import models
 
-class Item(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
+class MyDjangoTests(TestCase):
+    class Item(models.Model):
+        name = models.CharField(max_length=100)
+        description = models.TextField()
 
-    def __str__(self):
-        return self.name
+        def __str__(self):
+            return self.name
+
+    def setUp(self):
+        self.item = self.Item.objects.create(name='Test Item', description='This is a test item.')
+
+    def test_item_str(self):
+        self.assertEqual(str(self.item), 'Test Item')
+
+    def test_index_view(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Items')
+        self.assertTemplateUsed(response, 'index.html')
